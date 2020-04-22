eae1474c82e43a37bb9265e08c50164f04caf83c
Mark Olbert <mark@arcabama.com>
2020-04-22 12:37:14 -0700

    Added unit test project. Added test for binding to a subproperty.

---
+++ UnitTests/MainClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+    public class SubClass
+    {
+        public int IntProperty { get; set; }
+    }
+
+    public class MainClass
+    {
+        public string TextProperty { get; set; }
+        public SubClass Sub { get; set; } = new SubClass();
+    }
+}
---
+++ UnitTests/SubPropertyBindingTestCase.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.CommandLine;
+using System.CommandLine.Binding;
+using System.CommandLine.Builder;
+using System.CommandLine.Invocation;
+using System.CommandLine.Parsing;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class SubPropertyBindingTestCase
+    {
+        [TestMethod]
+        public void Bind_to_subproperty()
+        {
+            var mainOption = new Option<string>("-t");
+            var subOption = new Option<int>("-i");
+
+            var modelBinder = new ModelBinder<MainClass>();
+            modelBinder.BindMemberFromValue(mc => mc.TextProperty, mainOption);
+            modelBinder.BindMemberFromValue(mc => mc.Sub.IntProperty, subOption);
+
+            var mainClass = new MainClass();
+
+            var cmdLine = "-t sometext -i 27";
+            var args = CommandLineStringSplitter.Instance
+                .Split( cmdLine )
+                .ToList();
+
+            var builder = new CommandLineBuilder()
+                .UseDefaults()
+                .UseMiddleware( ic =>
+                {
+                    modelBinder.UpdateInstance(mainClass, ic.BindingContext);
+                } );
+
+            var parser = builder.Build();
+            var parseResults = parser.Parse( args );
+        }
+    }
+}
