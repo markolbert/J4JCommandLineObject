9445487c92604ac34b280dbd56b0a58974c385b5
Mark Olbert <mark@arcabama.com>
2020-04-24 19:51:29 -0700

    First pass at implementing property paths for binding to expressions appears to work.

--- UnitTests/MainClass.cs
+++ UnitTests/MainClass.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using J4JSoftware.CommandLine;
 
 namespace UnitTests
 {
@@ -11,9 +12,20 @@ namespace UnitTests
         public int IntProperty { get; set; }
     }
 
-    public class MainClass
+    public class MainClass : RootObjectModel
     {
         public string TextProperty { get; set; }
         public SubClass Sub { get; set; } = new SubClass();
+
+        protected override void DefineBindings( IObjectBinder objBinder )
+        {
+            var binder = (ObjectBinder<MainClass>) objBinder;
+
+            binder.AddOption(mc => mc.Sub.IntProperty, "-i")
+                .DefaultValue(-5);
+
+            binder.AddOption( mc => mc.TextProperty, "-t" )
+                .DefaultValue( "ralph" );
+        }
     }
 }
--- UnitTests/SubPropertyBindingTestCase.cs
+++ UnitTests/SubPropertyBindingTestCase.cs
@@ -3,6 +3,7 @@ using System.CommandLine;
 using System.CommandLine.Binding;
 using System.CommandLine.Builder;
 using System.CommandLine.Invocation;
+using System.CommandLine.IO;
 using System.CommandLine.Parsing;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -12,32 +13,12 @@ namespace UnitTests
     [TestClass]
     public class SubPropertyBindingTestCase
     {
-        [TestMethod]
+        [ TestMethod ]
         public void Bind_to_subproperty()
         {
-            var mainOption = new Option<string>("-t");
-            var subOption = new Option<int>("-i");
-
-            var modelBinder = new ModelBinder<MainClass>();
-            modelBinder.BindMemberFromValue(mc => mc.TextProperty, mainOption);
-            modelBinder.BindMemberFromValue(mc => mc.Sub.IntProperty, subOption);
-
             var mainClass = new MainClass();
 
-            var cmdLine = "-t sometext -i 27";
-            var args = CommandLineStringSplitter.Instance
-                .Split( cmdLine )
-                .ToList();
-
-            var builder = new CommandLineBuilder()
-                .UseDefaults()
-                .UseMiddleware( ic =>
-                {
-                    modelBinder.UpdateInstance(mainClass, ic.BindingContext);
-                } );
-
-            var parser = builder.Build();
-            var parseResults = parser.Parse( args );
+            mainClass.Initialize("-t sometext -i 27", new TestConsole() );
         }
     }
 }
